b872af5abdbaf84e75e4c0a17ebea2f6f89f4a9a
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-03-26 03:52:03 +0800

    indent

--- client/main.go
+++ client/main.go
@@ -6,20 +6,20 @@ import (
 )
 
 func main() {
-    // Connect to the server
-    conn, err := net.Dial("tcp", "localhost:8080")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+  // Connect to the server
+  conn, err := net.Dial("tcp", "localhost:8080")
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
-    // Send some data to the server
-    _, err = conn.Write([]byte("Hello, server!"))
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+  // Send some data to the server
+  _, err = conn.Write([]byte("Hello, server!"))
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
 
-    // Close the connection
-    conn.Close()
+  // Close the connection
+  conn.Close()
 }
